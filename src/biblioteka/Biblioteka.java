ca95b1ccbd64b8e65b2b1fc3c49a9239eee04076
bojantomic <tomicb@fon.rs>
2022-03-09 08:56:32 +0100

    Napravljena klasa Biblioteka koja implementira BibliotekaInterfejs

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,38 @@
+package biblioteka;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	private List<Knjiga> knjige = new LinkedList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		List<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for (Knjiga k : knjige)
+			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase()))
+				rezultat.add(k);
+		
+		return rezultat;
+	}
+
+}
